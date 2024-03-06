edae7e65bf40a20706c77ab4c98111043b94ab5a
siddhantsingh-25 <siddhant000004@gmail.com>
2024-03-06 09:01:58 -0500

    module3 completed

--- server/database/app.js
+++ server/database/app.js
@@ -59,23 +59,47 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find({state: req.params.state});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
+
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find({id: req.params.id});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
+
 });
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+//   const documents = await Reviews.find().sort( { id: -1 } )
+  const documents = await Reviews.aggregate([
+    { $sort: { id: -1 } },
+    { $limit: 1 }
+  ]).toArray();
+  let new_id = documents[0]['id']+1;
 
   const review = new Reviews({
 		"id": new_id,
